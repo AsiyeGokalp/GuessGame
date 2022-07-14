9466d50d44e94a277d345f0f7f072627ad3c7f5d
AsiyeGokalp <107327598+AsiyeGokalp@users.noreply.github.com>
2022-07-14 23:05:39 +0200

    bg color changed

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ var msg2=document.getElementById("message2");
 var msg3=document.getElementById("message3");
 
 var answer=Math.floor(Math.random()*100)+1;
+
 var turn=0;
 var yourGuess=[];
 
 function play(){
+ 
+ 
   var userGuess=document.getElementById("guess").value;
+  
   if(userGuess <1 || userGuess >100){
     alert("Please enter a number between 1 and 100!");
   }else{
     yourGuess.push(userGuess);
+    
     turn += 1 ;
+    
 
     if (userGuess < answer){
       msg1.textContent = "Your guess is too low" ;
       msg2.textContent = "turn : " +turn;
       msg3.textContent = "Guessed numbers are: "+yourGuess;
+      
+      
       
     }
     else if (userGuess > answer){
@@ -33,6 +41,7 @@ function play(){
       msg3.textContent = "You guessed it in "+turn+ " guesses";
       document.getElementById(myBtn).disabled=true;
     }
+    
   }
-    userGuess.value="";
+ 
 }
